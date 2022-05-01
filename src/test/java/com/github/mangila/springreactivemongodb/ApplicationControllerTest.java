f02195ea34801f4d8de8d3621e9658bbee41a993
Mangila <olsson.erik1993@gmail.com>
2022-05-01 20:34:42 +0200

    remove unused import

--- src/test/java/com/github/mangila/springreactivemongodb/ApplicationControllerTest.java
+++ src/test/java/com/github/mangila/springreactivemongodb/ApplicationControllerTest.java
@@ -1,7 +1,6 @@
 package com.github.mangila.springreactivemongodb;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
